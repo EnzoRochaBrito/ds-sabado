f6410807c3d5b689a88b61c4f04e29816399e19d
EnzoRochaBrito <151791091+EnzoRochaBrito@users.noreply.github.com>
2025-03-08 15:53:02 +0000

    salvando arquivos da aula de sábado

---
+++ projeto/Condicionais.java
@@ -0,0 +1,16 @@
+public class Condicionais {
+    public static void main(String[] args) {
+
+        int a = 18;
+        int b = 2;
+        int maior;
+
+        if (a>b){
+            maior = a;
+        } else {
+            maior = b;
+        }
+
+        System.out.println("O maior número é: " + maior);
+    }
+}
---
+++ projeto/Variaveis.java
@@ -0,0 +1,16 @@
+public class Variaveis {
+    public static void main(String[] args) {
+
+        String nome = "Marta";
+        int idade = 23;
+        double peso = 60.200;
+
+        final String CPF = "123.456.789-00";
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Peso: " + peso);
+        System.out.println("CPF: " + CPF);
+
+    }
+}
